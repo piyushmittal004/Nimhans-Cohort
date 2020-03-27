a098c1828df8e0036f80c1165ac6e4cd3e8fe44e
piyushmittal004 <piyush.mittal004@gmail.com>
2020-03-27 14:10:32 +0530

    Add files via upload

---
+++ src/app/User.ts
@@ -0,0 +1,46 @@
+
+export interface User{
+    //  position:number;
+    //  name:string;
+    //  weight:number;
+    //  symbol:string;
+
+
+ADBS_ID:number;
+ Assessment_ID:number;
+ D_no:string;
+ Family_no:number;
+p_no:number;
+ p_name:string;
+date_of_consent:Date;
+ index_cohort:string;
+lab_group:string;
+remarks:string;
+date_of_assessment:Date;
+Assessed_by:String;
+psw:String;
+pdf:String;
+consultant_incharge:String;
+sociodemography:boolean;
+ HOPI:boolean;
+Developmental:boolean;
+physical_exam:boolean;
+ MSE:boolean;
+Life_chart:boolean;
+Treatment:boolean;
+DSM5CC:boolean;
+Pedigree:boolean;
+FIGS:boolean;
+MINI:boolean;
+ ASRS:boolean;
+HMSE:boolean;
+CGI_S:boolean;
+Document_verified_by_PDF:string;
+Date_verified_by_pdf:Date;
+Document_verified_by_cohort:string;
+Date_verified_by_cohort:string;
+Comments:string;
+ Updated_verification_by:string;
+Update_verification_date:Date;
+
+}
